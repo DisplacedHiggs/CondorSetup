6a1f4f1379937656101a29fd19e629e76c7c9341
Matthew Walker <mwalker@physics.rutgers.edu>
2016-10-26 16:10:36 -0500

    updating createEfficiency for multiple tasks

--- createEfficiency.sh
+++ createEfficiency.sh
@@ -11,7 +11,7 @@ cd $CMSSW_BASE
 eval `scramv1 runtime -sh`
 
 cd $CMSSW_BASE/..
-tar  --exclude-caches-all -czf ${SCRIPTDIR}/${CMSSW_VERSION}.tar.gz ${CMSSW_VERSION}/
+#tar  --exclude-caches-all -czf ${SCRIPTDIR}/${CMSSW_VERSION}.tar.gz ${CMSSW_VERSION}/
 cd $MAINDIR
 
 runScript=$SCRIPTDIR/runEfficiency.sh
@@ -44,69 +44,77 @@ processlist=processes.list
 while read process
 do
     sample=`echo $process | sed 's/--//g'`
-    
+    echo $sample
     isSignal=false
     isMC=false
     jsonFile="Cert_271036-279588_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt"
     mode=0
 
-    dir1=`eos root://cmseos.fnal.gov ls -l /store/group/lpchbb/mwalker/2016/DisplacedDijet/${process}/ | awk '{print $9}'`
-    dir2=`eos root://cmseos.fnal.gov ls -l /store/group/lpchbb/mwalker/2016/DisplacedDijet/${process}/${dir1}/ | awk '{print $9}'`
-    dir3=`eos root://cmseos.fnal.gov ls -l /store/group/lpchbb/mwalker/2016/DisplacedDijet/${process}/${dir1}/${dir2}/ | awk '{print $9}'`
-    tmplist2=tmp.tmp
-    echo $dir3 | tr " " "\n" > $tmplist2
-    #cat $tmplist2
     OUTDIR=/store/group/lpchbb/mwalker/AnalysisTrees/${sample}/
     eos root://cmseos.fnal.gov mkdir $OUTDIR
 
-    while read subdir
+    dir1=`eos root://cmseos.fnal.gov ls -l /store/group/lpchbb/mwalker/2016/DisplacedDijet/${process}/ | awk '{print $9}'`
+    dir2=`eos root://cmseos.fnal.gov ls -l /store/group/lpchbb/mwalker/2016/DisplacedDijet/${process}/${dir1}/ | awk '{print $9}'`
+    tmplist2=tmp2.tmp
+    echo $dir2| tr " " "\n" > $tmplist2
+    while read subdir2
     do
-	indir=/store/group/lpchbb/mwalker/2016/DisplacedDijet/${process}/${dir1}/${dir2}/${subdir}/
-	filelist=${process}_${subdir}.list
 
-	if [ -e $filelist ]
-	then
-	    rm -rf $filelist
-	fi
+	dir3=`eos root://cmseos.fnal.gov ls -l /store/group/lpchbb/mwalker/2016/DisplacedDijet/${process}/${dir1}/${subdir2}/ | awk '{print $9}'`
+	tmplist3=tmp3.tmp
+	echo $dir3 | tr " " "\n" > $tmplist3
 
-	eos root://cmseos.fnal.gov ls -l $indir | grep root | awk -v indir="root://cmseos.fnal.gov/$indir" '{print indir $9}' > $filelist
-    
-	condorFile=$SCRIPTDIR/submitEfficiency_${sample}_${subdir}.condor
-        
-	if [ -e $condorFile ]
-	then
-	    rm -rf $condorFile
-	fi
-	touch $condorFile
-    
-	echo "universe = vanilla" >> $condorFile
-	echo 'Requirements = OpSys == "LINUX" && (Arch != "DUMMY" )' >> $condorFile
-	echo "Executable = $runScript" >> $condorFile
-	echo "Should_Transfer_Files = YES" >> $condorFile
-	echo "request_disk = 10000000" >> $condorFile
-	echo "request_memory = 2100" >> $condorFile
-	echo "WhenTOTransferOutput  = ON_EXIT_OR_EVICT" >> $condorFile
-	echo "Notification=never" >> $condorFile
-	echo "notify_user = mwalker@physics.rutgers.edu" >> $condorFile
-	echo 'x509userproxy = $ENV(X509_USER_PROXY)' >> $condorFile
-	echo "Transfer_Input_Files = runEfficiency.sh, ${CMSSW_VERSION}.tar.gz, dijetAnalysis.C, helperDisplacedDijets.C, helperDisplacedDijetTriggers.C,$jsonFile" >> $condorFile
-	echo "" >> $condorFile
-	echo "" >> $condorFile
-	i=0
-
-	while read line
+	while read subdir
 	do
-	    base=`echo $line | awk '{split($1,array,"results_"); split(array[2],array2,".");print array2[1]}'`
-	    ifname=$line
-	    mode=0
-	    ofile=histo_${sample}_${base}.root
-	    echo "output = $LOGDIR/\$(Cluster)_efficiency_${sample}_${base}.out" >> $condorFile
-	    echo "error = $LOGDIR/\$(Cluster)_efficiency_${sample}_${base}.err" >> $condorFile
-	    echo "log = $LOGDIR/\$(Cluster)_efficiency_${sample}_${base}.log" >> $condorFile
-	    echo "arguments = $ifname $ofile $isSignal $isMC $OUTDIR $jsonFile $mode" >> $condorFile
-	    echo "queue" >> $condorFile
+	    indir=/store/group/lpchbb/mwalker/2016/DisplacedDijet/${process}/${dir1}/${subdir2}/${subdir}/
+	    echo $indir
+	    filelist=${process}_${subdir2}_${subdir}.list
+
+	    if [ -e $filelist ]
+	    then
+		rm -rf $filelist
+	    fi
+
+	    eos root://cmseos.fnal.gov ls -l $indir | grep root | awk -v indir="root://cmseos.fnal.gov/$indir" '{print indir $9}' > $filelist
+    
+	    condorFile=$SCRIPTDIR/submitEfficiency_${sample}_${subdir2}_${subdir}.condor
+        
+	    if [ -e $condorFile ]
+	    then
+		rm -rf $condorFile
+	    fi
+	    touch $condorFile
+    
+	    echo "universe = vanilla" >> $condorFile
+	    echo 'Requirements = OpSys == "LINUX" && (Arch != "DUMMY" )' >> $condorFile
+	    echo "Executable = $runScript" >> $condorFile
+	    echo "Should_Transfer_Files = YES" >> $condorFile
+	    echo "request_disk = 10000000" >> $condorFile
+	    echo "request_memory = 2100" >> $condorFile
+	    echo "WhenTOTransferOutput  = ON_EXIT_OR_EVICT" >> $condorFile
+	    echo "Notification=never" >> $condorFile
+	    echo "notify_user = mwalker@physics.rutgers.edu" >> $condorFile
+	    echo 'x509userproxy = $ENV(X509_USER_PROXY)' >> $condorFile
+	    echo "Transfer_Input_Files = runEfficiency.sh, ${CMSSW_VERSION}.tar.gz, dijetAnalysis.C, helperDisplacedDijets.C, helperDisplacedDijetTriggers.C,$jsonFile" >> $condorFile
 	    echo "" >> $condorFile
-	done < $filelist
+	    echo "" >> $condorFile
+	    i=0
+	    
+	    while read line
+	    do
+		base=`echo $line | awk '{split($1,array,"results_"); split(array[2],array2,".");print array2[1]}'`
+		ifname=$line
+		mode=0
+		ofile=histo_${sample}_${subdir2}_${base}.root
+		echo "output = $LOGDIR/\$(Cluster)_efficiency_${sample}_${subdir2}_${base}.out" >> $condorFile
+		echo "error = $LOGDIR/\$(Cluster)_efficiency_${sample}_${subdir2}_${base}.err" >> $condorFile
+		echo "log = $LOGDIR/\$(Cluster)_efficiency_${sample}_${subdir2}_${base}.log" >> $condorFile
+		echo "arguments = $ifname $ofile $isSignal $isMC $OUTDIR $jsonFile $mode" >> $condorFile
+		echo "queue" >> $condorFile
+		echo "" >> $condorFile
+	    done < $filelist
+	done < $tmplist3
+	rm $tmplist3
     done < $tmplist2
     rm $tmplist2
 done < $processlist
